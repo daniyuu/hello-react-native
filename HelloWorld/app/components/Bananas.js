2f7ac51a8261c11fa21b32026ace759b12de75e2
Kagura Chen <yueych@microsoft.com>
2017-01-16 18:20:51 +0800

    react-web

--- HelloWorld/app/components/Bananas.js
+++ HelloWorld/app/components/Bananas.js
@@ -7,13 +7,37 @@ import {
     Image
 } from 'react-native';
 
+class Greeting extends Component {
+    render() {
+        return (
+            <Text>Hello {this.props.name}</Text>
+        );
+    }
+}
+
+class LotsOfGreetings extends Component {
+    render() {
+        return (
+            <View style={{alignItems: 'center'}}>
+                <Greeting name='Rexxar'/>
+                <Greeting name='Jaina'/>
+                <Greeting name='Valeera'/>
+            </View>
+        )
+    }
+}
+
 export default class Bananas extends Component {
     render() {
         let pic = {
             uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
         };
         return (
-            <Image source={pic} style={{width: 193, height: 110}}/>
+            <View>
+                <Image source={pic} style={{width: 193, height: 110}}/>
+                <LotsOfGreetings/>
+            </View>
+
         );
     }
 }
